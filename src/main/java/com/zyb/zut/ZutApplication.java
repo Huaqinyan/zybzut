a4ff72f324fcf6dfef3b3bee2120b5a71e9cfbf6
zybheart <647918763@qq.com>
2021-12-31 12:48:01 +0800

    2-2 init

---
+++ src/main/java/com/zyb/zut/ZutApplication.java
@@ -0,0 +1,13 @@
+package com.zyb.zut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyb/zut/ZutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyb.zut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
