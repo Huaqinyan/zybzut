62e423cdbb314e053b1a2f7b1e92f08a6983ecbc
zybheart <647918763@qq.com>
2021-12-31 22:01:54 +0800

    2-7 使用http测试接口

--- src/main/java/com/zyb/zut/controller/TestController.java
+++ src/main/java/com/zyb/zut/controller/TestController.java
@@ -1,9 +1,7 @@
 package com.zyb.zut.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 //@ComponentScan("com.zyb")
 //@ComponentScan({"com.zyb",con.test}) //扫描不和主类在同一级的类
 @RestController
@@ -20,5 +18,10 @@ public class TestController {
     @GetMapping("/hello")//只支持get请求
     public String hello(){
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")//只支持get请求
+    public String hellopost(String name){
+        return "Hello World!"+name;
     }
 }
