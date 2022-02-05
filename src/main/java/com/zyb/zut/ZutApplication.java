3b2e34fd34f04946d5d92a1024ee2847a67cc1b9
zybheart <647918763@qq.com>
2022-02-05 21:33:40 +0800

    2-5 修改启动文案

--- src/main/java/com/zyb/zut/ZutApplication.java
+++ src/main/java/com/zyb/zut/ZutApplication.java
@@ -1,13 +1,27 @@
 package com.zyb.zut;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+// @ComponentScan({"com.zyb", "com.test"})
+@ComponentScan("com.zyb")
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class ZutApplication {
 
+    private static final Logger LOG= (Logger) LoggerFactory.getLogger(ZutApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(ZutApplication.class, args);
+        SpringApplication app=new SpringApplication(ZutApplication.class);
+        Environment env= (Environment) app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
