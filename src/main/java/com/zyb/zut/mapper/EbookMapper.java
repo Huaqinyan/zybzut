94bab1b9e9f09acc019e6e88ed4b54f96f27055c
zybheart <647918763@qq.com>
2022-02-10 22:36:37 +0800

    3-6新增ebook表，生成持久层代码

---
+++ src/main/java/com/zyb/zut/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package com.zyb.zut.mapper;
+
+import com.zyb.zut.domain.Ebook;
+import com.zyb.zut.domain.EbookExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
