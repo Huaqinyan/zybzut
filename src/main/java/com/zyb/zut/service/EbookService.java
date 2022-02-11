ec0ce0d8284fc5bc8cd1aafb3f5e02150b0cc3e8
zybheart <647918763@qq.com>
2022-02-11 15:47:56 +0800

    3-8 制作Copyutil封装BeanUtils

--- src/main/java/com/zyb/zut/service/EbookService.java
+++ src/main/java/com/zyb/zut/service/EbookService.java
@@ -5,11 +5,10 @@ import com.zyb.zut.domain.EbookExample;
 import com.zyb.zut.mapper.EbookMapper;
 import com.zyb.zut.req.EbookReq;
 import com.zyb.zut.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.zyb.zut.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,12 +22,16 @@ public class EbookService {
         criteria.andNameLike("%"+ebookReq.getName()+"%");
         List<Ebook> ebookList= ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList=new ArrayList<>();
-        for (Ebook ebook:ebookList){
-            EbookResp ebookResp=new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList=new ArrayList<>();
+//        for (Ebook ebook:ebookList){
+////            EbookResp ebookResp=new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            对象复制
+//            EbookResp ebookResp= CopyUtil.copy(ebook,EbookResp.class);
+//
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
