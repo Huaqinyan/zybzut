98c9c8e12d971649fc2d8052703c2f2dcdbb9a08
zybheart <647918763@qq.com>
2022-02-12 20:38:50 +0800

    5-4电子书布局调整,一行显示三列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -8,7 +8,7 @@
                 <a-sub-menu key="sub1">
                     <template #title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 subnav 1
               </span>
                     </template>
@@ -20,7 +20,7 @@
                 <a-sub-menu key="sub2">
                     <template #title>
               <span>
-                <laptop-outlined />
+                <laptop-outlined/>
                 subnav 2
               </span>
                     </template>
@@ -32,7 +32,7 @@
                 <a-sub-menu key="sub3">
                     <template #title>
               <span>
-                <notification-outlined />
+                <notification-outlined/>
                 subnav 3
               </span>
                     </template>
@@ -46,49 +46,34 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-            <template #footer>
-                <div>
-                    <b>ant design vue</b>
-                    footer part
-                </div>
-            </template>
-            <template #renderItem="{ item }">
-                <a-list-item key="item.title">
-                    <template #actions>
-      <span v-for="{ type, text } in actions" :key="type">
-        <component v-bind:is="type" style="margin-right: 8px" />
-        {{ text }}
-      </span>
-                    </template>
-                    <template #extra>
-                        <img
-                                width="272"
-                                alt="logo"
-                                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                        />
-                    </template>
-                    <a-list-item-meta :description="item.description">
-                        <template #title>
-                            <a :href="item.href">{{ item.title }}</a>
+            <a-list item-layout="vertical" size="large" :grid="{ gutter: 20,column: 3 }" :data-source="ebooks">
+
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.name">
+                        <template #actions>
+                            <span v-for="{ type, text } in actions" :key="type">
+                                <component v-bind:is="type" style="margin-right: 8px"/>
+                                {{ text }}
+                            </span>
                         </template>
-                        <template #avatar><a-avatar :src="item.avatar" /></template>
-                    </a-list-item-meta>
-                    {{ item.content }}
-                </a-list-item>
-            </template>
-        </a-list>
-    <pre>{{ebooks}}
-      {{ebooks2}}
-    </pre>
-
-
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.name }}</a>
+                            </template>
+                            <template #avatar>
+                                <a-avatar :src="item.cover"/>
+                            </template>
+                        </a-list-item-meta>
+                        {{ item.content }}
+                    </a-list-item>
+                </template>
+            </a-list>
         </a-layout-content>
     </a-layout>
 </template>
 
 <script lang="ts">
-    import {defineComponent,onMounted,ref,reactive,toRef} from 'vue';
+    import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
     import axios from 'axios';
 
     const listData: Record<string, string>[] = [];
@@ -107,34 +92,34 @@
 
 
     export default defineComponent({
-        name:'Home',
-        setup(){
+        name: 'Home',
+        setup() {
             console.log("setup");
-            const ebooks=ref();
-            const ebooks1=reactive({books:[]});
-            onMounted(()=>{
+            const ebooks = ref();
+            const ebooks1 = reactive({books: []});
+            onMounted(() => {
                 console.log("onMounted");
-                axios.get("http://localhost:8080/ebook/list?name=Vue").then((response)=> {
-                    const data=response.data;
-                    ebooks.value=data.content;
-                    ebooks1.books=data.content;
+                axios.get("http://localhost:8080/ebook/list?name=Vue").then((response) => {
+                    const data = response.data;
+                    ebooks.value = data.content;
+                    ebooks1.books = data.content;
                     console.log(response);
                 });
             });
             return {
                 ebooks,
-                ebooks2: toRef(ebooks1,"books"),
+                ebooks2: toRef(ebooks1, "books"),
                 listData,
-                pagination:{
-                    onChange:(page:any)=>{
+                pagination: {
+                    onChange: (page: any) => {
                         console.log(page);
                     },
-                    pagesize:3,
+                    pagesize: 3,
                 },
-                actions:[
-                    {type: 'StarOutlined',text: '156'},
-                    {type: 'LikeOutlined',text: '156'},
-                    {type: 'MessageOutlined',text: '2'},
+                actions: [
+                    {type: 'StarOutlined', text: '156'},
+                    {type: 'LikeOutlined', text: '156'},
+                    {type: 'MessageOutlined', text: '2'},
                 ],
             }
         }
