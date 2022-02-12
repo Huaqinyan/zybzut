498dd43e08047c562622d89944690f53979500f0
zybheart <647918763@qq.com>
2022-02-12 21:40:16 +0800

    5-5 axios请求域改为读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,8 +98,8 @@
             const ebooks = ref();
             const ebooks1 = reactive({books: []});
             onMounted(() => {
-                console.log("onMounted");
-                axios.get("http://localhost:8080/ebook/list").then((response) => {
+                console.log("onMounted1");
+                axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then((response) => {
                     const data = response.data;
                     ebooks.value = data.content;
                     ebooks1.books = data.content;
