b35516ff11984fdc347d1de4a8553ec063bbbd18
zybheart <647918763@qq.com>
2022-03-25 14:51:40 +0800

    7-5父分类改为下拉选择

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -61,6 +61,17 @@
             </a-form-item>
             <a-form-item label="父分类">
                 <a-input v-model:value="category.parent" />
+                <a-select
+                        ref="select"
+                        v-model:value="category.parent"
+                        style="width: 120px"
+                        @focus="focus"
+                >
+                    <a-select-option value="0">无</a-select-option>
+                    <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id===c.id">
+                        {{c.name}}
+                    </a-select-option>
+                </a-select>
             </a-form-item>
             <a-form-item label="顺序">
                 <a-input v-model:value="category.sort" />
